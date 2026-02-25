7771af905ea59ef5fba30fe0acc30bc19cf02ccd
Nicolas KLEIN <nicolas6720@gmail.com>
2026-02-25 21:02:31 +0100

    feat : added rofi configuration

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ mkdir ~/.config
 ln -sf ~/dotfiles/.config/hypr ~/.config/hypr
 ln -sf ~/dotfiles/.config/waybar ~/.config/waybar
 ln -sf ~/dotfiles/.config/dunst ~/.config/dunst
+ln -sf ~/dotfiles/.config/rofi ~/.config/rofi
 
 echo "Done."
